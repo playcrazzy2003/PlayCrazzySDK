f5ee02d691d675b0230e5d321fef6660fe51294d
ItEven <aryankumawat281004@gmail.com>
2025-05-05 10:46:59 +0530

    Update

--- Assets/_PlayCrazzy/Dev/_Scripts/BaseUnlocker/BaseUnlockerTable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "BaseUnlockerTable", menuName = "Game/Base Unlocker Table")]
-public class BaseUnlockerTable : ScriptableObject
-{
-    public List<BaseUnlockerEntry> entries = new List<BaseUnlockerEntry>();
-}
